0ce0f384044b0408aa5b18615e0a49f86c7f1f9e
reza <reza.ghasemi8664@gmail.com>
2026-09-16 10:04:48 +0430

    fix: use shared Prisma client in message routes

--- backend/routes/message.routes.cjs
+++ backend/routes/message.routes.cjs
@@ -2,8 +2,8 @@
 
 const express = require('express');
 const router = express.Router();
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const prismaModule = require('../config/prisma.cjs');
+const prisma = prismaModule.prisma || prismaModule;
 
 const { verifyToken } = require('../middlewares/auth.middleware.cjs');
 
