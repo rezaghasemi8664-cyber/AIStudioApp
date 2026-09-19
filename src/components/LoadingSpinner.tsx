878398acc8059de4171166b6de953f69552dced2
rezaghasemi8664-cyber <reza.ghasemi8664@gmail.com>
2026-09-19 13:33:32 +0330

    style: standardize loading spinner workstation

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const LoadingSpinner: React.FC = () => (
-    <div className="flex justify-center items-center h-full min-h-[300px]">
-        <div className="w-12 h-12 border-4 border-t-transparent border-cyan-500 rounded-full animate-spin"></div>
+    <div className="roniya-loading-spinner flex justify-center items-center h-full min-h-[300px]">
+        <div className="roniya-loading-spinner-ring w-12 h-12 border-4 border-t-transparent border-cyan-500 rounded-full animate-spin"></div>
     </div>
 );
 
